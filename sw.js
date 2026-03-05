31d7a948a21cfff50d8b503180eddf2f69f81c6c
morpheus45 <52168974+morpheus45@users.noreply.github.com>
2026-03-05 10:39:43 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 /* VOD PWA - simple offline cache (GitHub Pages friendly) */
-const CACHE_NAME = "vod-pwa-cache-v1";
+const CACHE_NAME = "vod-pwa-cache-v1-v2";
 const ASSETS = [
   "./",
   "./index.html",
   "./admin.html",
-  "./panneau.html",
+  "./panel.html",
   "./player.html",
   "./app.js",
   "./vitrine.js",
@@ -18,7 +18,7 @@ const ASSETS = [
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS))
+      .then((cache) => Promise.allSettled(ASSETS.map((u)=>cache.add(u))))
       .then(() => self.skipWaiting())
   );
 });
