37559300fdfbe751de960b9c68ffdb5b8861e9bc
morpheus45 <52168974+morpheus45@users.noreply.github.com>
2026-04-28 23:07:17 +0200

    Update SUPABASE_ANON key in auth.js

--- auth.js
+++ auth.js
@@ -9,7 +9,7 @@
 //  https://supabase.com → New project → Settings → API
 // ─────────────────────────────────────────────────────────────────
 const SUPABASE_URL  = "https://gwmuazostbbgroplnlql.supabase.co";
-const SUPABASE_ANON = "sb_secret_7UU8xG7kdoFmPqVier-52w_piCsBvtisb_secret_7UU8xG7kdoFmPqVier-52w_piCsBvti";
+const SUPABASE_ANON = "sb_secret_7UU8xG7kdoFmPqVier-52w_piCsBvti";
 //qaehOyHFvPymhtua
 // E-mail du compte admin (accès illimité, panel admin visible)
 const ADMIN_EMAIL   = "cedric.lago@gmail.com";
