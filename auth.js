3b26dcc9eb544e32384f67cfdb98774169df3691
morpheus45 <52168974+morpheus45@users.noreply.github.com>
2026-04-28 23:12:06 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@
 //  CONFIG SUPABASE — à remplir après création du projet
 //  https://supabase.com → New project → Settings → API
 // ─────────────────────────────────────────────────────────────────
-const SUPABASE_URL  = https://gwmuazostbbgroplnlql.supabase.co ;
+const SUPABASE_URL  = https://gwmuazostbbgroplnlql.supabase.co/rest/v1/ ;
 const SUPABASE_ANON = sb_secret_7UU8xG7kdoFmPqVier-52w_piCsBvti;
 //qaehOyHFvPymhtua
 // E-mail du compte admin (accès illimité, panel admin visible)
