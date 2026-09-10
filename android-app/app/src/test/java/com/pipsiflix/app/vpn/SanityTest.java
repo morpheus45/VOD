f01849aadadd6972b4c0c7221ea31e8bee92d55c
morpheus45 <cedric.lago@gmail.com>
2026-09-10 11:12:03 +0200

    chore(vpn): setup dépendance WireGuard + test infra + manifeste + gitignore configs

---
+++ android-app/app/src/test/java/com/pipsiflix/app/vpn/SanityTest.java
@@ -0,0 +1,8 @@
+package com.pipsiflix.app.vpn;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class SanityTest {
+    @Test public void harnessRuns() { assertTrue(true); }
+}
